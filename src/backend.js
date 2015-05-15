f4d5cc00674780b39ef78411bf472fce612159b6
Nathan Tate <nathantate88@gmail.com>
2015-05-15 12:37:27 -0600

    Backend error

--- src/backend.js
+++ src/backend.js
@@ -8,10 +8,15 @@ export default class BackendAccess {
   getAccessor(req, res) {
     return _.mapValues(this.expressHandler.controllers, (Controller)=> {
       return _.mapValues(Controller.getAllRoutes('express'), (value, key)=> {
-        return function(...args) {
+        return async function(...args) {
           let controller = new Controller(key);
           controller.overrideData = args;
-          return controller.runExpressRoute(this.request, this.response);
+          let result = await controller.runExpressRoute(this.request, this.response);
+          if(result.success) {
+            return result.result;
+          } else {
+            throw result.error;
+          };
         };
       });
     });
