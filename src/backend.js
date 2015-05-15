9d15684be6bccbfc5fb162bd21ddd47e0e32d3e9
Nathan Tate <nathantate88@gmail.com>
2015-05-15 12:33:29 -0600

    Fixed backend bug

--- src/backend.js
+++ src/backend.js
@@ -7,16 +7,13 @@ export default class BackendAccess {
   }
   getAccessor(req, res) {
     return _.mapValues(this.expressHandler.controllers, (Controller)=> {
-      return _.reduce(Controller.getAllRoutes('express'), (obj, value, key)=> {
-        Object.defineProperty(obj, key, {
-          get: function(...args) {
-            let controller = new Controller(key);
-            controller.overrideData = args;
-            return controller.runExpressRoute(this.request, this.response);
-          }
-        });
-        return obj;
-      }, {});
+      return _.mapValues(Controller.getAllRoutes('express'), (value, key)=> {
+        return function(...args) {
+          let controller = new Controller(key);
+          controller.overrideData = args;
+          return controller.runExpressRoute(this.request, this.response);
+        };
+      });
     });
   }
 }
